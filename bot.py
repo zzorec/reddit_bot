ecff783f73dbbed94af49a037d481f472fa905ec
Cerozz <zoki.zan@gmail.com>
2025-02-24 12:25:20 +0100

    Moved logs to correct logger.

--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-import logging
 import os
 import sys
 import threading
@@ -16,9 +15,9 @@ def run_inter_bot() -> None:
 
     if find_dotenv():
         load_dotenv()  # Load environment variables from .env file if it exists.
-        logging.info("Loaded environment variables from .env file.")
+        logger.info("Loaded environment variables from .env file.")
     else:
-        logging.warning("No .env file found. Using Linux environment variables.")
+        logger.warning("No .env file found. Using Linux environment variables.")
 
     # Check environment variables.
     required_env_vars: Final[list[str]] = ["RAPID_API_KEY", "REDDIT_CLIENT_ID", "REDDIT_CLIENT_SECRET", "REDDIT_USERNAME", "REDDIT_PASSWORD", "REDDIT_USER_AGENT", "REDDIT_SUBREDDIT_NAME", "REDDIT_APPROVED_USER_LIST"]
@@ -31,13 +30,13 @@ def run_inter_bot() -> None:
             missing_env_vars.append(var)
         else:
             if var in os.environ:
-                logging.info(f"Using {var} from Linux environment variable.")
+                logger.info(f"Using {var} from Linux environment variable.")
             else:
-                logging.info(f"Using {var} from .env file.")
+                logger.info(f"Using {var} from .env file.")
 
     if missing_env_vars:
         for env_var in missing_env_vars:
-            logging.error(f"Environment variable {env_var} is not set!")
+            logger.error(f"Environment variable {env_var} is not set!")
         sys.exit(1)  # Exit the program if any of the required environment variables are missing
 
     # Creates a Reddit instance.
@@ -52,27 +51,27 @@ def run_inter_bot() -> None:
     # Create a thread with a comment stream. It will immediately pick up any new comment in the comment stream.
     t1 = threading.Thread(target=reddit_comment_util.process_comments_organizer, args=(reddit_instance,))
     t1.start()
-    logging.info("Started thread 1 for process_comments_organizer.")
+    logger.info("Started thread 1 for process_comments_organizer.")
 
     # Create a thread with submission processing. Unlike with comments, this is not implemented with native streaming capability due to inability to implement removal flair feature. It works by fetching batches of latest posts.
     t2 = threading.Thread(target=reddit_submission_util.process_submissions_organizer, args=(reddit_instance,))
     t2.start()
-    logging.info("Started thread 2 for process_submissions_organizer.")
+    logger.info("Started thread 2 for process_submissions_organizer.")
 
     # Create a thread with upcoming match thread analyzer that will check for any upcoming games and create pre-match and match threads.
     t3 = threading.Thread(target=reddit_match_thread_util.match_threads_creator, args=(reddit_instance,))
     t3.start()
-    logging.info("Started thread 3 for match_threads_creator.")
+    logger.info("Started thread 3 for match_threads_creator.")
 
     # Create a thread for updating match threads when a match is live.
     t4 = threading.Thread(target=reddit_match_thread_util.match_threads_updater, args=(reddit_instance,))
     t4.start()
-    logging.info("Started thread 4 for match_threads_updater.")
+    logger.info("Started thread 4 for match_threads_updater.")
 
     # Create a thread for updating subreddit's sidebar.
     t5 = threading.Thread(target=reddit_sidebar_util.sidebar_updater, args=(reddit_instance,))
     t5.start()
-    logging.info("Started thread 5 for sidebar_updater.")
+    logger.info("Started thread 5 for sidebar_updater.")
 
 
 def main() -> None:
